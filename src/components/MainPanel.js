2ce03e1ac0ed6e31b08f96a6815aedfe89500c50
Leo Fabrikant <leofab86@gmail.com>
2016-10-29 12:53:44 -0400

    destination dialogue

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -2,27 +2,37 @@
 var React = require('react');
 var csInterface = new window.CSInterface();
 
-var cssStore = [];
-var i=0;
-var cssdata = "";  
-var csspath = window.React.SystemPath+'/Adobe/CEP/extensions/React/exports/test.css';
+  
 var htmlTopPath = window.React.SystemPath+'/Adobe/CEP/extensions/React/src/codesnippets/top.html';
 var htmlBotPath = window.React.SystemPath+'/Adobe/CEP/extensions/React/src/codesnippets/bottom.html';
-var htmlFinalPath = window.React.SystemPath+'/Adobe/CEP/extensions/React/exports/final.html';
 var htmltop = window.cep.fs.readFile(htmlTopPath).data;
 var htmlbot = window.cep.fs.readFile(htmlBotPath).data;
-var body = '';
+
 
 
 
 var MainPanel = React.createClass({
 
-	testScript: function() {
+	addDocument: function() {
 		csInterface.evalScript("addDocument()");
+
 	},
 
-	saveCss: function() {
-		csInterface.addEventListener('documentCreated', function(event) {
+	exportWebsite: function() {
+		var cssStore = [];
+		var i=0;
+		var cssdata = "";
+		var body = '';
+		var csspath = '';
+		var htmlFinalPath = '';
+
+		csInterface.evalScript("exportLayer()");
+		csInterface.addEventListener('exportInfo', function(event){
+			csspath = event.data + '/stylesheet.css';
+			htmlFinalPath = event.data + '/index.html';
+		});
+
+		csInterface.addEventListener('outputLayerCSS', function(event) {
 			cssStore[i] = event.data;
 			i++
 
@@ -31,14 +41,14 @@ var MainPanel = React.createClass({
 
 			var classId = event.data.split(/\.| /);
 
-			body = body + "\n<div class='" + classId[1] + "'></div>";
+			body = body + "\t\t<div class='" + classId[1] + "'></div>\n";
 
-			var htmlFinal = htmltop + body + '\n' + htmlbot;
+			var htmlFinal = htmltop + body + htmlbot;
 
 			window.cep.fs.writeFile(htmlFinalPath, htmlFinal);
 
-
 		});
+
 		csInterface.evalScript("iterate()");
 		
 	},
@@ -47,10 +57,9 @@ var MainPanel = React.createClass({
 		return (
 			<div>
 				<h1>React Extension</h1>
-				<button onClick={this.testScript}>New Document</button>
+				<button onClick={this.addDocument}>New Document</button>
 				<br/><br/>
-				<button onClick={this.saveCss}>Export to HTML / CSS</button>
-				<p>Files exported to export folder</p>
+				<button onClick={this.exportWebsite}>Export a Website</button>
 			</div>
 		);
 	}
